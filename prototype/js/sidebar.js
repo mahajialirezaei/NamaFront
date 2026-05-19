15ad7531177589a58d26c9d95ac6564d46fb5cd2
Mohammad <m.a.hajialirezaei05@gmail.com>
2026-05-20 00:01:17 +0330

    chore: complete sidebar.js

--- prototype/js/sidebar.js
+++ prototype/js/sidebar.js
@@ -3,10 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
         'index.html': { url: 'index.html', menuText: 'Home' },
         'channel.html': { url: 'channel.html', menuText: 'My Channel' },
         'admin.html': { url: 'admin.html', menuText: 'Admin panel' },
-        'trending': { url: null, menuText: 'Trending', notReady: true },
-        'subscriptions': { url: null, menuText: 'Subscriptions', notReady: true },
-        'library': { url: null, menuText: 'Library', notReady: true },
-        'history': { url: null, menuText: 'History', notReady: true }
+        'history.html': { url: 'history.html', menuText: 'History' },
+        'trending.html': { url: 'trending.html', menuText: 'Trending' },
+        'subscriptions.html': { url: 'subscriptions.html', menuText: 'Subscriptions' },
+        'library.html': { url: 'library.html', menuText: 'Library' }
     };
 
     let currentFile = window.location.pathname.split('/').pop();
@@ -37,10 +37,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 if (cleanText === 'Home') destination = 'index.html';
                 else if (cleanText === 'My Channel') destination = 'channel.html';
                 else if (cleanText === 'Admin panel') destination = 'admin.html';
-                else if (cleanText === 'Trending') destination = null;
-                else if (cleanText === 'Subscriptions') destination = null;
-                else if (cleanText === 'Library') destination = null;
-                else if (cleanText === 'History') destination = null;
+                else if (cleanText === 'Trending') destination = 'trending.html';
+                else if (cleanText === 'Subscriptions') destination = 'subscriptions.html';
+                else if (cleanText === 'Library') destination = 'library.html';
+                else if (cleanText === 'History') destination = 'history.html';
 
                 if (destination) {
                     if (window.location.pathname.includes(destination)) return;
@@ -56,6 +56,10 @@ document.addEventListener('DOMContentLoaded', function() {
         if (currentFile === 'index.html') highlightMenuItem('Home');
         else if (currentFile === 'channel.html') highlightMenuItem('My Channel');
         else if (currentFile === 'admin.html') highlightMenuItem('Admin panel');
+        else if (currentFile === 'history.html') highlightMenuItem('History');
+        else if (currentFile === 'trending.html') highlightMenuItem('Trending');
+        else if (currentFile === 'subscriptions.html') highlightMenuItem('Subscriptions');
+        else if (currentFile === 'library.html') highlightMenuItem('Library');
         else {
             highlightMenuItem('Home');
         }
